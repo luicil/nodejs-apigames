fd07b4ec2f420c6b4a5d25db189c286541396d42
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-15 17:45:54 -0300

    Axios

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 80;
+const cors = require("cors");
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 //app.use(express.static("public"));
 
 var DB ={
